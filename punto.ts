eceec8ffdaf14b207d907148b78ca3baa7c69188
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-17 15:23:53 +0100

    Punto hasta el 10

---
+++ punto.ts
@@ -0,0 +1,61 @@
+export class Punto {
+    private x: number;
+    private y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    //metodos
+    public tostring(): string {
+        let x: string = this.x.toString()
+        let y: string = this.y.toString()
+        let resultado: string = "(" + x + "," + y + ")"
+        return resultado
+    }
+    public distaciaAlOrigen(): number {
+        let suma: number = Math.pow(this.x, 2) + Math.pow(this.y, 2)
+        let resultado: number = Math.sqrt(suma);
+        return resultado
+    }
+
+    public calcularDistancia(otroPunto: Punto): number {
+        let cuadradoX: number = Math.pow(otroPunto.getX() - this.x, 2)
+        let cuadradoY: number = Math.pow(otroPunto.getY() - this.y, 2)
+        let suma: number = cuadradoX + cuadradoY;
+        let raiCuadrada: number = Math.sqrt(suma);
+        return raiCuadrada
+    }
+    public calcularCuadrante(): number {
+        let resultado: number = 0
+        if (this.x == 0 && this.y == 0) {
+            resultado;
+        } else if (Math.sign(this.x) == 1 && Math.sign(this.y) == 1) {
+            resultado = 1;
+        } else if (Math.sign(this.x) == -1 && Math.sign(this.y) == 1) {
+            resultado = 2;
+        } else if (Math.sign(this.x) == -1 && Math.sign(this.y) == -1) {
+            resultado = 3;
+        } else if (Math.sign(this.x) == 1 && Math.sign(this.y) == -1) {
+            resultado = 4;
+        }
+        return resultado
+    }
+    public calcularMasCercano(puntos:Punto[]){
+        
+    }
+    public getX(): number {
+        return this.x;
+    }
+
+    public setX(x: number) {
+        this.x = x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+    public setY(y: number) {
+        this.y = y;
+    }
+
+}
